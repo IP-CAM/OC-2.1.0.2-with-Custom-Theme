60e7aa7f72c46f413a709d15e6decdab40285335
Dang Tam <danghatam@gmail.com>
2016-02-28 13:35:51 -0500

    installed

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://giaocucnhanh.com/admin/');
+define('HTTP_CATALOG', 'http://giaocucnhanh.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://giaocucnhanh.com/admin/');
+define('HTTPS_CATALOG', 'http://giaocucnhanh.com/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/giaocucnhanh.com/public_html/admin/');
+define('DIR_SYSTEM', '/var/www/giaocucnhanh.com/public_html/system/');
+define('DIR_LANGUAGE', '/var/www/giaocucnhanh.com/public_html/admin/language/');
+define('DIR_TEMPLATE', '/var/www/giaocucnhanh.com/public_html/admin/view/template/');
+define('DIR_CONFIG', '/var/www/giaocucnhanh.com/public_html/system/config/');
+define('DIR_IMAGE', '/var/www/giaocucnhanh.com/public_html/image/');
+define('DIR_CACHE', '/var/www/giaocucnhanh.com/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/giaocucnhanh.com/public_html/system/storage/download/');
+define('DIR_LOGS', '/var/www/giaocucnhanh.com/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/giaocucnhanh.com/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/giaocucnhanh.com/public_html/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/giaocucnhanh.com/public_html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '7582013');
+define('DB_DATABASE', 'giaocucnhanh');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://giaocucnhanh.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://giaocucnhanh.com/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/giaocucnhanh.com/public_html/catalog/');
+define('DIR_SYSTEM', '/var/www/giaocucnhanh.com/public_html/system/');
+define('DIR_LANGUAGE', '/var/www/giaocucnhanh.com/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/giaocucnhanh.com/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/giaocucnhanh.com/public_html/system/config/');
+define('DIR_IMAGE', '/var/www/giaocucnhanh.com/public_html/image/');
+define('DIR_CACHE', '/var/www/giaocucnhanh.com/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/giaocucnhanh.com/public_html/system/storage/download/');
+define('DIR_LOGS', '/var/www/giaocucnhanh.com/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/giaocucnhanh.com/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/giaocucnhanh.com/public_html/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '7582013');
+define('DB_DATABASE', 'giaocucnhanh');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
